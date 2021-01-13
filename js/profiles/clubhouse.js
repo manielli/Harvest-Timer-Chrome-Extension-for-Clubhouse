e3c86ba9acaf461f06b364f0cde7eea21fb779d4
Mani Jafari <mani@laterolabs.com>
2021-01-13 10:51:27 -0800

    Add the epic name in brakets and story number in curly brackets in notes field of Harvest timer modal

--- js/profiles/clubhouse.js
+++ js/profiles/clubhouse.js
@@ -5,9 +5,15 @@
   injectScript = function(opts) {
     var name, ph, script, value;
     script = document.createElement('script');
-    for (name in opts) {
-      value = opts[name];
-      script.setAttribute(name, value);
+    switch (typeof opts) {
+      case 'object':
+        for (name in opts) {
+          value = opts[name];
+          script.setAttribute(name, value);
+        }
+        break;
+      case 'string':
+        script.innerHTML = opts;
     }
     ph = document.getElementsByTagName('script')[0];
     return ph.parentNode.insertBefore(script, ph);
@@ -18,8 +24,10 @@
       this.host = host1;
       this.addTimerIfOnStory = bind(this.addTimerIfOnStory, this);
       this.handleMutations = bind(this.handleMutations, this);
-      this.infect();
+      this.actionSelector = '.story-attributes';
+      this.platformLoaded = false;
       this.listen();
+      this.infect();
     }
 
     ClubhouseProfile.prototype.platformConfig = function() {
@@ -29,37 +37,25 @@
     };
 
     ClubhouseProfile.prototype.listen = function() {
-      document.body.addEventListener('harvest-event:ready', this.addTimerIfOnStory);
+      document.body.addEventListener('harvest-event:ready', (function(_this) {
+        return function() {
+          _this.platformLoaded = true;
+          return _this.addTimerIfOnStory();
+        };
+      })(this));
       this.harvestTimerButton = this.createButton();
-      this.harvestTimerButton.classList.add('attributes editable-attribute');
+      this.harvestTimerButton.classList.add('attribute', 'editable-attribute');
 
-      return new MutationObserver(this.handleMutations).observe(document.body, {
-        childList: true,
-        subtree: true
-      });
-    };
-
-    ClubhouseProfile.prototype.handleMutations = function(mutations) {
-      var i, len, node, removedNodes, results;
-      results = [];
-      for (i = 0, len = mutations.length; i < len; i++) {
-        removedNodes = mutations[i].removedNodes;
-        results.push((function() {
-          var j, len1, results1;
-          results1 = [];
-          for (j = 0, len1 = removedNodes.length; j < len1; j++) {
-            node = removedNodes[j];
-            if (this.hasBeenRemoved(node, this.harvestTimerButton)) {
-              results1.push(this.addTimerIfOnStory());
-            } else {
-              results1.push(void 0);
-            }
+      return window.addEventListener("message", (function(_this) {
+        return function(event) {
+          if (event.data.clubhouseUrlChanged == null) {
+            return;
           }
-          return results1;
-        }).call(this));
-      }
-      return results;
-    };
+          return _this.addTimerIfOnStory();
+        };
+      })(this))
+
+    }
 
     ClubhouseProfile.prototype.hasBeenRemoved = function(node, button) {
       return node.contains(button) && !document.body.contains(button) && button !== node;
@@ -71,23 +67,76 @@
         'data-platform-config': JSON.stringify(this.platformConfig()),
         async: true
       });
-      return document.addEventListener('pjax:end', this.addTimerIfOnStory);
+      return injectScript("(" + this.clubhouseUrlMonitor + ")();")
     };
+
+    ClubhouseProfile.prototype.clubhouseUrlMonitor = function() {
+      var change, fn;
+      change = function() {
+        return window.postMessage({
+          clubhouseUrlChanged: true
+        }, "*");
+      };
+      fn = window.history.pushState;
+      window.history.pushState = function() {
+        fn.apply(window.history, arguments);
+        return change();
+      };
+      return window.addEventListener("popstate", change);
+    }
 
     ClubhouseProfile.prototype.addTimerIfOnStory = function() {
       var _, workspace, storyNumber, storyTitleHyphened, ref;
       ref = window.location.pathname.split('/'), _ = ref[0], workspace = ref[1], story = ref[2], storyNumber = ref[3], storyTitleHyphened = ref[4];
-      if (!(story && story === 'story' && storyNumber !== 'space')) {
+      if (!(story && story === 'story')) {
         return;
       }
-      return this.addTimer({
-        workspace: workspace,
-        storyNumber: '[' + storyNumber + ']',
-        storyTitle: this.storyTitle(),
-        epicName: this.epicName(),
-        projectName: this.projectName()
-      });
+      return this.whenReadyForTimer((function(_this) {
+        return function() {
+          return _this.addTimer({
+            workspace: {
+              id: workspace,
+              workspace: workspace
+            },
+            storyNumber: {
+              id: storyNumber,
+              storyNumber: storyNumber
+            },
+            storyTitle: {
+              id: _this.storyTitle(),
+              storyTitle: _this.storyTitle()
+            },
+            epicName: {
+              id: _this.epicName(),
+              epicName: _this.epicName()
+            },
+            projectName: {
+              id: _this.projectName(),
+              projectName: _this.projectName()
+            },
+            item: {
+              id: storyNumber,
+              name: (_this.epicName() !== 'None' ? '[' + _this.epicName() + ']' : '' ) + '(' + _this.storyNumber() + ') ' + _this.storyTitle()
+            },
+          })
+        }
+      })(this));
     };
+
+    ClubhouseProfile.prototype.whenReadyForTimer = function(callback) {
+      var poll;
+      poll = (function(_this) {
+        return function() {
+          if (!document.querySelector(_this.actionSelector)) {
+            return;
+          }
+          window.clearInterval(_this.interval);
+          return callback();
+        };
+      })(this);
+      window.clearInterval(this.interval);
+      return this.interval = window.setInterval(poll, 200);
+    }
 
     ClubhouseProfile.prototype.storyTitle = function() {
       var ref;
@@ -110,7 +159,7 @@
     };
 
     ClubhouseProfile.prototype.addTimer = function(data) {
-      var workspace, storyNumber, storyTitle, epicName, projectName, name, actions, permalink;
+      var workspace, storyNumber, storyTitle, epicName, projectName, name, actions, permalink, item;
 
       for (name in data) {
         this.harvestTimerButton.dataset[name] = JSON.stringify(data[name]);
@@ -125,10 +174,12 @@
 
       this.harvestTimerButton.removeAttribute('data-listening');
       this.harvestTimerButton.setAttribute('data-permalink', permalink);
+      this.harvestTimerButton.classList.remove('disabled');
+      this.harvestTimerButton.classList.add('harvest-timer');
 
-      actions = document.querySelector('.story-attributes')
+      actions = document.querySelector("div.story-attributes");
       if (actions != null) {
-        actions.append(this.harvestTimerButton);
+        actions.insertBefore(this.harvestTimerButton, actions.children[7]);
       }
 
       return this.notifyPlatformOfNewTimers();
@@ -138,9 +189,9 @@
       var button;
       button = document.createElement('button');
       button.type = 'button';
-      button.classList.add('harvest-timer');
+      button.classList.add('harvest-timer', 'attribute', 'editable-attribute');
       button.setAttribute('data-skip-styling', 'true');
-      button.innerHTML = "<svg aria-hidden=\"true\" class=\"octicon octicon-clock\" height=\"16\" role=\"img\" version=\"1.1\" viewBox=\"0 0 14 16\" width=\"14\"><path d=\"M8 8h3v2H7c-0.55 0-1-0.45-1-1V4h2v4z m-1-5.7c3.14 0 5.7 2.56 5.7 5.7S10.14 13.7 7 13.7 1.3 11.14 1.3 8s2.56-5.7 5.7-5.7m0-1.3C3.14 1 0 4.14 0 8s3.14 7 7 7 7-3.14 7-7S10.86 1 7 1z\"></path></svg>\nTrack time";
+      button.innerHTML = "<svg aria-hidden=\"true\" class=\"octicon octicon-clock\" height=\"16\" role=\"img\" version=\"1.1\" viewBox=\"0 0 14 16\" width=\"14\"><path d=\"M8 8h3v2H7c-0.55 0-1-0.45-1-1V4h2v4z m-1-5.7c3.14 0 5.7 2.56 5.7 5.7S10.14 13.7 7 13.7 1.3 11.14 1.3 8s2.56-5.7 5.7-5.7m0-1.3C3.14 1 0 4.14 0 8s3.14 7 7 7 7-3.14 7-7S10.86 1 7 1z\"></path></svg>\nTrack with Harvest";
       return button;
     }
 
